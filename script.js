155848ed8e7a5d3a76a73801b1b20c4a534bcc2a
Himanshu <github.me268@gmail.com>
2020-07-03 02:53:30 +0530

    added animations

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+// var number = 1987;
+// for (var i = 0; i < number; i++) {
+// 	setInterval(function () {
+// 		$("#card-number-main").html(i);
+// 	}, 500)
+// }
+$('.count').each(function () {
+  var $this = $(this);
+  jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
+    duration: 1000,
+    easing: 'swing',
+    step: function () {
+      $this.text(Math.ceil(this.Counter));
+    }
+  });
+});
 $('#dark-mode').change(function(){
     if($(this).is(':checked')) {
         $("body, .lt-dark-text, .card, .header-mine").addClass("dt");
